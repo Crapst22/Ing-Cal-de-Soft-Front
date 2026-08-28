00a7e4293993a346e818c2d10a8740f37637b9ee
Valentino <valentino.pajon@gmail.com>
2026-08-27 21:54:36 -0300

    cambio de id cleinte porque no se si ya lo habian tocado

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { CatalogosProvider } from "./context/catalogos-context.tsx";
 
 
 createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider clientId="1056435830719-183eq4etd5ic0aq6k89jglhn33bpvigh.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="145763953821-7kop2u3no3g56g244l0c49g7m34vqd14.apps.googleusercontent.com">
     <StrictMode>
       <ConfiguracionSistemaProvider>
         <FiltrosProvider>
